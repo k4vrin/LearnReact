3fd8489f8d3cf63aea4f3929c1f1c285630da91f
Mostafa Hosseini <kavrindev@gmail.com>
2025-07-24 20:52:30 +0330

    refactor: restructure App component and add Header; introduce MainGoal component

---
+++ 01-starting-project/src/components/MainGoal.jsx
@@ -0,0 +1,10 @@
+export function MainGoal() {
+    return (
+        <div>
+            <h2>My main goal</h2>
+            <p>
+                Learn React in-depth and from the ground up
+            </p>
+        </div>
+    )
+}
